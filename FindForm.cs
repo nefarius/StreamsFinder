2755fb816f3d48d1f4efcd9d67cc18b218b73d8e
Benjamin Höglinger <nefarius@dhmx.at>
2011-05-24 15:56:40 +0000

    Corrected a few bugs and made search code thread-safe

--- FindForm.cs
+++ FindForm.cs
@@ -22,93 +22,92 @@
 //
 //************************************************************************************
 using System;
-using System.Drawing;
 using System.Collections;
-using System.ComponentModel;
-using System.Windows.Forms;
+using System.Collections.Generic;
 using System.Data;
 using System.IO;
-using NTFS;
 using System.Threading;
+using System.Windows.Forms;
 
 namespace CNRService.StreamsFinder
 {
-	/// <summary>
-	/// Descrizione di riepilogo per Form1.
-	/// </summary>
-	public class FindForm : System.Windows.Forms.Form
-	{
-		private System.Windows.Forms.Panel panel1;
-		private System.Windows.Forms.DataGrid dataGridResult;
-		private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
-		private System.Windows.Forms.Label label1;
-		private System.Windows.Forms.Button buttonFind;
-		private System.Windows.Forms.TextBox textBoxFind;
-		private System.Windows.Forms.Button buttonBrowse;
-		private System.ComponentModel.IContainer components;
-		private CNRService.StreamsFinder.FileInfoData fileInfoData1;
-		private System.Windows.Forms.DataGridTableStyle dataGridTableStyle1;
-		private System.Windows.Forms.DataGridTextBoxColumn dataGridTextBoxColumn1;
-		private System.Windows.Forms.DataGridTextBoxColumn dataGridTextBoxColumn2;
-		private System.Windows.Forms.DataGridTextBoxColumn dataGridTextBoxColumn3;
-		private System.Windows.Forms.DataGridTextBoxColumn dataGridTextBoxColumn4;
+    /// <summary>
+    /// Descrizione di riepilogo per Form1.
+    /// </summary>
+    public class FindForm : System.Windows.Forms.Form
+    {
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGrid dataGridResult;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonFind;
+        private System.Windows.Forms.TextBox textBoxFind;
+        private System.Windows.Forms.Button buttonBrowse;
+        private System.ComponentModel.IContainer components;
+        private CNRService.StreamsFinder.FileInfoData fileInfoData1;
+        private System.Windows.Forms.DataGridTableStyle dataGridTableStyle1;
+        private System.Windows.Forms.DataGridTextBoxColumn dataGridTextBoxColumn1;
+        private System.Windows.Forms.DataGridTextBoxColumn dataGridTextBoxColumn2;
+        private System.Windows.Forms.DataGridTextBoxColumn dataGridTextBoxColumn3;
+        private System.Windows.Forms.DataGridTextBoxColumn dataGridTextBoxColumn4;
         private System.Windows.Forms.DataGridTextBoxColumn dataGridTextBoxColumn5;
         private System.Windows.Forms.Splitter splitter1;
 
-		private Thread SearchThread;
-		private System.Windows.Forms.Timer timerGrid;
+        private Thread SearchThread;
+        private System.Windows.Forms.Timer timerGrid;
 
-		public struct FileInfoStruct
-		{
-			public string File_Name;
-			public string Stream_Name;
-			public long Stream_Size;
-			public string Location;
-			public System.DateTime Creation_Date;
-		}
+        public struct FileInfoStruct
+        {
+            public string File_Name;
+            public string Stream_Name;
+            public long Stream_Size;
+            public string Location;
+            public System.DateTime Creation_Date;
+        }
 
-		private ArrayList ArrayFileInfo;
-		private System.Windows.Forms.CheckBox checkBoxSubFolders;
+        private ArrayList ArrayFileInfo;
+        private System.Windows.Forms.CheckBox checkBoxSubFolders;
         private Button buttonSelectAll;
         private Button buttonRemoveSelected;
-		private int lastIndexAdded = 0;
+        private int lastIndexAdded = 0;
 
-		public FindForm()
-		{
-			//
-			// Necessario per il supporto di Progettazione Windows Form
-			//
-			InitializeComponent();
+        public FindForm()
+        {
+            //
+            // Necessario per il supporto di Progettazione Windows Form
+            //
+            InitializeComponent();
 
-			ArrayFileInfo = new ArrayList();
-		}
+            ArrayFileInfo = new ArrayList();
+        }
 
-		/// <summary>
-		/// Pulire le risorse in uso.
-		/// </summary>
-		protected override void Dispose( bool disposing )
-		{
-			if( disposing )
-			{
-				if (components != null) 
-				{
-					components.Dispose();
-				}
-			}
-			base.Dispose( disposing );
-		}
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (components != null)
+                {
+                    components.Dispose();
+                }
+            }
+            base.Dispose(disposing);
+        }
 
-		#region Codice generato da Progettazione Windows Form
-		/// <summary>
-		/// Metodo necessario per il supporto della finestra di progettazione. Non modificare
-		/// il contenuto del metodo con l'editor di codice.
-		/// </summary>
-		private void InitializeComponent()
-		{
+        #region Codice generato da Progettazione Windows Form
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FindForm));
             this.textBoxFind = new System.Windows.Forms.TextBox();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonRemoveSelected = new System.Windows.Forms.Button();
             this.buttonSelectAll = new System.Windows.Forms.Button();
             this.checkBoxSubFolders = new System.Windows.Forms.CheckBox();
             this.label1 = new System.Windows.Forms.Label();
@@ -125,7 +124,6 @@ namespace CNRService.StreamsFinder
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.splitter1 = new System.Windows.Forms.Splitter();
             this.timerGrid = new System.Windows.Forms.Timer(this.components);
-            this.buttonRemoveSelected = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridResult)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.fileInfoData1)).BeginInit();
@@ -155,8 +153,20 @@ namespace CNRService.StreamsFinder
             this.panel1.Size = new System.Drawing.Size(792, 112);
             this.panel1.TabIndex = 1;
             // 
+            // buttonRemoveSelected
+            // 
+            this.buttonRemoveSelected.BackColor = System.Drawing.SystemColors.Control;
+            this.buttonRemoveSelected.Location = new System.Drawing.Point(207, 64);
+            this.buttonRemoveSelected.Name = "buttonRemoveSelected";
+            this.buttonRemoveSelected.Size = new System.Drawing.Size(153, 23);
+            this.buttonRemoveSelected.TabIndex = 8;
+            this.buttonRemoveSelected.Text = "Remove selected streams...";
+            this.buttonRemoveSelected.UseVisualStyleBackColor = true;
+            this.buttonRemoveSelected.Click += new System.EventHandler(this.buttonRemoveSelected_Click);
+            // 
             // buttonSelectAll
             // 
+            this.buttonSelectAll.BackColor = System.Drawing.SystemColors.Control;
             this.buttonSelectAll.Location = new System.Drawing.Point(97, 64);
             this.buttonSelectAll.Name = "buttonSelectAll";
             this.buttonSelectAll.Size = new System.Drawing.Size(104, 23);
@@ -294,16 +304,6 @@ namespace CNRService.StreamsFinder
             this.timerGrid.Interval = 300;
             this.timerGrid.Tick += new System.EventHandler(this.timerGrid_Tick);
             // 
-            // buttonRemoveSelected
-            // 
-            this.buttonRemoveSelected.Location = new System.Drawing.Point(207, 64);
-            this.buttonRemoveSelected.Name = "buttonRemoveSelected";
-            this.buttonRemoveSelected.Size = new System.Drawing.Size(153, 23);
-            this.buttonRemoveSelected.TabIndex = 8;
-            this.buttonRemoveSelected.Text = "Remove selected streams...";
-            this.buttonRemoveSelected.UseVisualStyleBackColor = true;
-            this.buttonRemoveSelected.Click += new System.EventHandler(this.buttonRemoveSelected_Click);
-            // 
             // FindForm
             // 
             this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
@@ -313,6 +313,7 @@ namespace CNRService.StreamsFinder
             this.Controls.Add(this.panel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FindForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Search for NTFS Streams";
             this.Load += new System.EventHandler(this.FindForm_Load);
             this.panel1.ResumeLayout(false);
@@ -321,177 +322,185 @@ namespace CNRService.StreamsFinder
             ((System.ComponentModel.ISupportInitialize)(this.fileInfoData1)).EndInit();
             this.ResumeLayout(false);
 
-		}
-		#endregion
+        }
+        #endregion
 
-		/// <summary>
-		/// Il punto di ingresso principale dell'applicazione.
-		/// </summary>
-		[STAThread]
-		static void Main() 
-		{
-			FindForm frm = new FindForm();
-			Application.Run(frm);
-		}
+        /// <summary>
+        /// Il punto di ingresso principale dell'applicazione.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            FindForm frm = new FindForm();
+            Application.EnableVisualStyles();
+            Application.Run(frm);
+        }
 
-		private void buttonFind_Click(object sender, System.EventArgs e)
-		{
-			if (SearchThread != null)
-			{
-				if (SearchThread.ThreadState == ThreadState.Running)
-					SearchThread.Abort();
-			}	
-			if (this.buttonFind.Text == "Stop search")
-			{
-					this.buttonFind.Text = "Start search";
-			}
-			else
-			{
-				this.buttonFind.Text = "Stop search";
-			
-				this.fileInfoData1.FileInfo.Rows.Clear();
-
-				Thread t = new Thread(new ThreadStart(FindFiles));
-				SearchThread = t;
-				SearchThread.Start();
-
-				Thread.Sleep(200);
-
-			}
-		}
-
-        private delegate void FindFilesCallback();
-		private void FindFiles()
-		{
-			DirSearch(this.textBoxFind.Text, this.checkBoxSubFolders.Checked);
-
-            if (!this.textBoxFind.InvokeRequired)
+        private void buttonFind_Click(object sender, System.EventArgs e)
+        {
+            if (SearchThread != null)
+            {
+                if (SearchThread.ThreadState == ThreadState.Running)
+                    SearchThread.Abort();
+            }
+            if (this.buttonFind.Text == "Stop search")
+            {
                 this.buttonFind.Text = "Start search";
+            }
             else
             {
+                this.buttonFind.Text = "Stop search";
+
+                this.fileInfoData1.FileInfo.Rows.Clear();
+
+                Thread t = new Thread(new ThreadStart(FindFiles));
+                SearchThread = t;
+                SearchThread.Start();
+
+                Thread.Sleep(200);
+
+            }
+        }
+
+        private delegate void FindFilesCallback();
+        private void FindFiles()
+        {
+            if (this.textBoxFind.InvokeRequired)
+            {
+                DirSearch(this.textBoxFind.Text, this.checkBoxSubFolders.Checked);
                 FindFilesCallback ff = new FindFilesCallback(FindFiles);
                 this.Invoke(ff);
             }
-		}
+            else
+            {
+                this.buttonFind.Text = "Start search";
+            }
+        }
 
-		private void DirSearch(string sDir, bool subFolders)
-		{
-			try
-			{
-				foreach (string f in Directory.GetFiles(sDir))
-				{	
-					FileInfo FSInfo = new FileInfo(f);
-					NTFS.FileStreams FS = new NTFS.FileStreams(f);
-						
-					foreach(NTFS.StreamInfo s in FS)
-					{
-						FileInfoStruct fis;
-						fis.File_Name = FS.FileName;
-						fis.Stream_Name = s.Name;
-						fis.Stream_Size = s.Size;
-						fis.Location = FSInfo.DirectoryName;
-						fis.Creation_Date = FSInfo.CreationTime;
+        private void DirSearch(string sDir, bool subFolders)
+        {
+            try
+            {
+                foreach (string f in Directory.GetFiles(sDir))
+                {
+                    FileInfo FSInfo = new FileInfo(f);
+                    NTFS.FileStreams FS = new NTFS.FileStreams(f);
 
-						ArrayFileInfo.Add(fis);
-					}	
-				}
-				// update the results label
-				String caption = "Results:  (" + this.fileInfoData1.FileInfo.Rows.Count.ToString() + ")";
-				this.dataGridResult.CaptionText = caption;
+                    foreach (NTFS.StreamInfo s in FS)
+                    {
+                        FileInfoStruct fis;
+                        fis.File_Name = FS.FileName;
+                        fis.Stream_Name = s.Name;
+                        fis.Stream_Size = s.Size;
+                        fis.Location = FSInfo.DirectoryName;
+                        fis.Creation_Date = FSInfo.CreationTime;
 
-				if (subFolders)
-				{
-					foreach (string d in Directory.GetDirectories(sDir))
-					{
-						DirSearch(d, subFolders);
-					}
-				}
-			}
-			catch
-			{
-			}
-		}
+                        ArrayFileInfo.Add(fis);
+                    }
+                }
+                // update the results label
+                String caption = "Results:  (" + this.ArrayFileInfo.Count.ToString() + ")";
+                this.dataGridResult.CaptionText = caption;
 
-		private void FindForm_Load(object sender, System.EventArgs e)
-		{
-			DataView dw = this.fileInfoData1.FileInfo.DefaultView;
-			dw.AllowEdit = false;
-			dw.AllowDelete = false;
-			dw.AllowNew = false;
-			this.dataGridResult.DataSource = dw;
-		}
+                if (subFolders)
+                {
+                    foreach (string d in Directory.GetDirectories(sDir))
+                    {
+                        DirSearch(d, subFolders);
+                    }
+                }
+            }
+            catch
+            {
+            }
+        }
 
-		private void RemoveSelectedStreams()
-		{
-			CurrencyManager cm = (CurrencyManager)this.BindingContext[dataGridResult.DataSource, dataGridResult.DataMember]; 
- 
-			DataView dv = (DataView)cm.List; 
-			for(int i = 0; i < dv.Count; ++i) 
-			{ 
-				if(dataGridResult.IsSelected(i)) 
-				{
-					FileInfoData.FileInfoRow r = (FileInfoData.FileInfoRow)dv[i].Row;
+        private void FindForm_Load(object sender, System.EventArgs e)
+        {
+            DataView dw = this.fileInfoData1.FileInfo.DefaultView;
+            dw.AllowEdit = false;
+            dw.AllowDelete = false;
+            dw.AllowNew = false;
+            this.dataGridResult.DataSource = dw;
+        }
 
-					NTFS.FileStreams FS = new NTFS.FileStreams(r.File_Name);
-				
-					foreach(NTFS.StreamInfo s in FS)
-					{
-						if (s.Name == r.Stream_Name)
-						{
-							s.Delete();
-						}
-					}
+        private void RemoveSelectedStreams()
+        {
+            CurrencyManager cm = 
+                (CurrencyManager)this.BindingContext[dataGridResult.DataSource, dataGridResult.DataMember];
+            List<FileInfoData.FileInfoRow> rowsToDelete = new List<FileInfoData.FileInfoRow>();
 
-                    
-				}
-			}
-		}
+            DataView dv = (DataView)cm.List;
+            for (int i = 0; i < dv.Count; ++i)
+            {
+                if (dataGridResult.IsSelected(i))
+                {
+                    FileInfoData.FileInfoRow r = (FileInfoData.FileInfoRow)dv[i].Row;
 
-		public ArrayList GetSelectedRows(DataGrid dg) 
-		{ 
-			ArrayList al = new ArrayList(); 
- 
-			CurrencyManager cm = (CurrencyManager)this.BindingContext[dg.DataSource, dg.DataMember]; 
- 
-			DataView dv = (DataView)cm.List; 
-			for(int i = 0; i < dv.Count; ++i) 
-			{ 
-				if(dg.IsSelected(i)) 
-					al.Add(i); 
-			}
-			return al; 
-		} 
- 
-		private void buttonBrowse_Click(object sender, System.EventArgs e)
-		{
-			if (this.folderBrowserDialog1.ShowDialog() == DialogResult.OK)
-				this.textBoxFind.Text = this.folderBrowserDialog1.SelectedPath;
-		}
+                    NTFS.FileStreams FS = new NTFS.FileStreams(r.File_Name);
 
-		private void timerGrid_Tick(object sender, System.EventArgs e)
-		{
-			this.timerGrid.Enabled = false;
+                    foreach (NTFS.StreamInfo s in FS)
+                    {
+                        if (s.Name == r.Stream_Name)
+                        {
+                            s.Delete();
+                        }
+                    }
 
-			int cnt = ArrayFileInfo.Count;
-			
+                    rowsToDelete.Add(r);
+                }
+            }
 
-			for(int i=lastIndexAdded; i<cnt; i++)
-			{
-				FileInfoStruct fis = (FileInfoStruct)ArrayFileInfo[i];
+            if (rowsToDelete.Count > 0)
+                foreach (FileInfoData.FileInfoRow tmpRow in rowsToDelete)
+                    tmpRow.Delete();
+        }
 
-				FileInfoData.FileInfoRow r = this.fileInfoData1.FileInfo.NewFileInfoRow();
-				r.BeginEdit();
-				r.File_Name = fis.File_Name;
-				r.Stream_Name = fis.Stream_Name;
-				r.Stream_Size = fis.Stream_Size;
-				r.Location = fis.Location;
-				r.Creation_Date = fis.Creation_Date;
-				r.EndEdit();
-				this.fileInfoData1.FileInfo.AddFileInfoRow(r);
-			}
-			lastIndexAdded = cnt;
-			this.timerGrid.Enabled = true;
-		}
+        public ArrayList GetSelectedRows(DataGrid dg)
+        {
+            ArrayList al = new ArrayList();
+
+            CurrencyManager cm = (CurrencyManager)this.BindingContext[dg.DataSource, dg.DataMember];
+
+            DataView dv = (DataView)cm.List;
+            for (int i = 0; i < dv.Count; ++i)
+            {
+                if (dg.IsSelected(i))
+                    al.Add(i);
+            }
+            return al;
+        }
+
+        private void buttonBrowse_Click(object sender, System.EventArgs e)
+        {
+            if (this.folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+                this.textBoxFind.Text = this.folderBrowserDialog1.SelectedPath;
+        }
+
+        private void timerGrid_Tick(object sender, System.EventArgs e)
+        {
+            this.timerGrid.Enabled = false;
+
+            int cnt = ArrayFileInfo.Count;
+
+
+            for (int i = lastIndexAdded; i < cnt; i++)
+            {
+                FileInfoStruct fis = (FileInfoStruct)ArrayFileInfo[i];
+
+                FileInfoData.FileInfoRow r = this.fileInfoData1.FileInfo.NewFileInfoRow();
+                r.BeginEdit();
+                r.File_Name = fis.File_Name;
+                r.Stream_Name = fis.Stream_Name;
+                r.Stream_Size = fis.Stream_Size;
+                r.Location = fis.Location;
+                r.Creation_Date = fis.Creation_Date;
+                r.EndEdit();
+                this.fileInfoData1.FileInfo.AddFileInfoRow(r);
+            }
+            lastIndexAdded = cnt;
+            this.timerGrid.Enabled = true;
+        }
 
         private void buttonSelectAll_Click(object sender, EventArgs e)
         {
@@ -511,5 +520,5 @@ namespace CNRService.StreamsFinder
             if (MessageBox.Show(msg, "Delete Streams", MessageBoxButtons.YesNo) == DialogResult.Yes)
                 RemoveSelectedStreams();
         }
-	}
+    }
 }
